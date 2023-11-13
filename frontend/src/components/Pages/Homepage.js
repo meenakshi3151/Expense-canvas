b59d8cb6a400e62a2196e9de1802ba3b6e2d0dc2
Shikhar Gupta <812shikhargupta@gmail.com>
2023-11-13 12:38:53 +0530

    Linking the main page with the Login Page

---
+++ frontend/src/components/Pages/Homepage.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useHistory } from "react-router";
+import Login from "../Authentication/Login";
+import Signup from "../Authentication/SignUp";
+
+function Homepage() {
+  const [selectedTab, setSelectedTab] = useState(0); // 0 for Login, 1 for Sign Up
+
+  const handleTabChange = (index) => {
+    setSelectedTab(index);
+  };
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Welcome!!!!
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded" index={selectedTab}>
+          <TabList mb="1em">
+            <Tab onClick={() => handleTabChange(0)}>Login</Tab>
+            <Tab onClick={() => handleTabChange(1)}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup handleTabChange={handleTabChange} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+}
+
+export default Homepage;
