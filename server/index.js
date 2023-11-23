b071bd89d2c819855a9585e45e456eb826ee50a7
Shikhar Gupta <812shikhargupta@gmail.com>
2023-11-23 18:23:09 +0530

    Socket.io server setUp

--- server/index.js
+++ server/index.js
@@ -54,3 +54,14 @@ const server = app.listen(
   PORT,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
+
+const io = require("socket.io")(server,{
+  pingTimeout:60000,
+  cors:{
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connecttion", (socket) => {
+  console.log("connected to socket.io");
+});
